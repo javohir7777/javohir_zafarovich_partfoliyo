3b66a34b84c23abdb77633762998f635d7b5ddca
javohir7777 <91466315+javohir7777@users.noreply.github.com>
2023-04-02 17:26:32 +0500

    ghjk
    hgjkl

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,14 +1,10 @@
 import Capture from "../assets/Capture.PNG";
 import Drergashov from "../assets/drergashov.PNG";
 import Namanganvxtb from "../assets/namanganvxtb.PNG";
-import Ifor from "../assets/ifor.PNG";
-import YoutubeClon from "../assets/youtubeclon.PNG";
 import Weather from "../assets/Weather.PNG";
 import Energiyat from "../assets/Energiyat.PNG";
 import Authorization from "../assets/authorization.PNG";
-import Medcare from "../assets/Medcare.PNG";
-import Calc from "../assets/Calc.PNG";
-import Payment from "../assets/payment.PNG";
+import Port from "../assets/portfolio.PNG";
 
 export const ProjectList = [
   {
@@ -24,32 +20,16 @@ export const ProjectList = [
     image: Energiyat,
     skills: ["ReactJS", "CSS", "Ant Design"],
     link: "https://javohirtecher1.netlify.app/",
-    github: "github.com/jamshid-zayniyev/Teacher1",
+    github: "https://github.com/javohir7777/Teacher1",
     domen: "javohirtecher1.netlify.app",
-  },
-  {
-    name: "New Payment",
-    image: Payment,
-    skills: ["ReactJS", "CSS", "Tailwind.css"],
-    link: "https://jammipay.vercel.app/",
-    github: "github.com/jamshid-zayniyev/jammipay",
-    domen: "jammipay.vercel.app",
   },
   {
     name: "Weather Today",
     image: Weather,
     skills: ["ReactJS", "CSS", "Bootstrap"],
     link: "https://weather-seven-chi.vercel.app/",
-    github: "github.com/jamshid-zayniyev/weather",
+    github: "https://github.com/javohir7777/weather",
     domen: "weather-seven-chi.vercel.app",
-  },
-  {
-    name: "Hospital Medcare Website",
-    image: Medcare,
-    skills: ["JavaScript", "Html", "Css"],
-    link: "http://jamshid-dev.lovestoblog.com/Hospital/index.html",
-    github: "Github source code is private",
-    domen: "Hospital.uz",
   },
   {
     name: "Namangan Viloyat XTB",
@@ -67,37 +47,20 @@ export const ProjectList = [
     github: "Github source code is private",
     domen: "drergashovdentalclinic.uz",
   },
-
-  {
-    name: "Online Shopping Website",
-    image: Ifor,
-    skills: ["ReactJS", "Redux", "CSS", "Bootstrap"],
-    link: "https://ifor-three.vercel.app/",
-    github: "github.com/jamshid-zayniyev/ifor",
-    domen: "ifor-three.vercel.app",
-  },
   {
     name: "Authorization example",
     image: Authorization,
     skills: ["ReactJS", "Redux", "CSS", "Bootstrap"],
     link: "https://authorization-ecru.vercel.app/",
-    github: "github.com/jamshid-zayniyev/Authorization",
+    github: "https://github.com/javohir7777/Authorization",
     domen: "authorization-ecru.vercel.app",
   },
   {
-    name: "Youtube Clone",
-    image: YoutubeClon,
-    skills: ["ReactJS", "Redux", "CSS", "Bootstrap"],
-    link: "https://jamzayn-tube.vercel.app/",
-    github: "github.com/jamshid-zayniyev/jamzayn-tube",
-    domen: "jamzayn-tube.vercel.app",
-  },
-  {
-    name: "Calculator",
-    image: Calc,
-    skills: ["ReactJS", "CSS"],
-    link: "https://jamzayncalculator.netlify.app/",
-    github: "github.com/jamshid-zayniyev/calculator",
-    domen: "jamzayncalculator.netlify.app",
+    name: "Portfolio web site",
+    image: Port,
+    skills: ["JavaScript", "Html", "Css"],
+    link: "https://jovohirportfolio2.netlify.app/index.html",
+    github: "https://github.com/javohir7777/portfolio_2",
+    domen: "jovohirportfolio2.netlify.app",
   },
 ];
